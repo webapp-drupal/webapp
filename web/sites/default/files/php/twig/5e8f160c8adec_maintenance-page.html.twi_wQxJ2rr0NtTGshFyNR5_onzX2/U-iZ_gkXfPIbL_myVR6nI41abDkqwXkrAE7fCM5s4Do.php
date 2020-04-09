155c051c4a94e7860c33252757076eb05b381059
WebApp Solutions <info@webapp-solutions.co.uk>
2020-04-09 13:04:20 +0000

    adminimal theme

---
+++ web/sites/default/files/php/twig/5e8f160c8adec_maintenance-page.html.twi_wQxJ2rr0NtTGshFyNR5_onzX2/U-iZ_gkXfPIbL_myVR6nI41abDkqwXkrAE7fCM5s4Do.php
@@ -0,0 +1,165 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bartik/templates/maintenance-page.html.twig */
+class __TwigTemplate_049fcd6d98bf94d6db47ab14ba0cd62ce98409c3585cd06316944f688ce3db77 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 15];
+        $filters = ["escape" => 20, "t" => 20];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 't'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 11
+        echo "<div id=\"page-wrapper\">
+  <div id=\"page\">
+    <header id=\"header\" role=\"banner\">
+      <div class=\"section clearfix\">
+        ";
+        // line 15
+        if ((($context["site_name"] ?? null) || ($context["site_slogan"] ?? null))) {
+            // line 16
+            echo "          <div id=\"name-and-slogan\"";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar((((($context["hide_site_name"] ?? null) && ($context["hide_site_slogan"] ?? null))) ? (" class=\"visually-hidden\"") : ("")));
+            echo ">
+            ";
+            // line 17
+            if (($context["site_name"] ?? null)) {
+                // line 18
+                echo "              <div id=\"site-name\"";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(((($context["hide_site_name"] ?? null)) ? (" class=\"visually-hidden\"") : ("")));
+                echo ">
+                <strong>
+                  <a href=\"";
+                // line 20
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["front_page"] ?? null)), "html", null, true);
+                echo "\" title=\"";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("Home"));
+                echo "\" rel=\"home\"><span>";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["site_name"] ?? null)), "html", null, true);
+                echo "</span></a>
+                </strong>
+              </div>
+            ";
+            }
+            // line 24
+            echo "            ";
+            if (($context["site_slogan"] ?? null)) {
+                // line 25
+                echo "              <div id=\"site-slogan\"";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(((($context["hide_site_slogan"] ?? null)) ? (" class=\"visually-hidden\"") : ("")));
+                echo ">
+                ";
+                // line 26
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["site_slogan"] ?? null)), "html", null, true);
+                echo "
+              </div>
+            ";
+            }
+            // line 29
+            echo "          </div>
+        ";
+        }
+        // line 31
+        echo "      </div>
+    </header>
+    <div id=\"main-wrapper\">
+      <div id=\"main\" class=\"clearfix\">
+        <main id=\"content\" class=\"column\" role=\"main\">
+          <section class=\"section\">
+            <a id=\"main-content\"></a>
+            ";
+        // line 38
+        if (($context["title"] ?? null)) {
+            // line 39
+            echo "              <h1 class=\"title\" id=\"page-title\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null)), "html", null, true);
+            echo "</h1>
+            ";
+        }
+        // line 41
+        echo "            ";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "content", [])), "html", null, true);
+        echo "
+            ";
+        // line 42
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed($this->getAttribute(($context["page"] ?? null), "highlighted", [])), "html", null, true);
+        echo "
+          </section>
+        </main>
+      </div>
+    </div>
+  </div>
+</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/maintenance-page.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  127 => 42,  122 => 41,  116 => 39,  114 => 38,  105 => 31,  101 => 29,  95 => 26,  90 => 25,  87 => 24,  76 => 20,  70 => 18,  68 => 17,  63 => 16,  61 => 15,  55 => 11,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bartik/templates/maintenance-page.html.twig", "/var/www/vhosts/webappsolutions.co.uk/httpdocs/dev/web/core/themes/bartik/templates/maintenance-page.html.twig");
+    }
+}
